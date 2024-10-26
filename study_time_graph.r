12b8d98cae964fa2660bc6a97e3184722210b9e3
Matt <mmtrapani@gmail.com>
2024-10-26 13:49:11 -0700

    added linter comments

--- study_time_graph.r
+++ study_time_graph.r
@@ -9,7 +9,7 @@ create_study_time_graph <- function(csv_file) {
   
   # Convert Study_Time to minutes
   log_data <- log_data %>%
-    mutate(Study_Time_Minutes = as.numeric(sapply(strsplit(Study_Time, ":"), function(x) {
+    mutate(Study_Time_Minutes = as.numeric(sapply(strsplit(Study_Time, ":"), function(x) { # nolint: object_usage_linter.
       as.numeric(x[1]) * 60 + as.numeric(x[2]) + as.numeric(x[3]) / 60
     })))
   
@@ -17,7 +17,7 @@ create_study_time_graph <- function(csv_file) {
   avg_study_time <- mean(log_data$Study_Time_Minutes, na.rm = TRUE)
   
   # Create the bar graph with average line
-  ggplot(log_data, aes(x = as.Date(Date), y = Study_Time_Minutes)) +
+  ggplot(log_data, aes(x = as.Date(Date), y = Study_Time_Minutes)) + # nolint: object_usage_linter.
     geom_bar(stat = "identity") +
     geom_hline(yintercept = avg_study_time, color = "red", linetype = "dashed") +
     labs(title = "Distribution of Study Times",
